72d936780a623e1d7d37497701f7427cc97f75d0
Deepak Sasi <Deepak.sasi@ltfs.com>
2018-06-14 14:40:31 +0530

    new changes by deepak

--- AboutUs.aspx.cs
+++ AboutUs.aspx.cs
@@ -5,9 +5,9 @@ using System.Data;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
+using System.IO;
 
 public partial class AboutUs : System.Web.UI.Page
 {
